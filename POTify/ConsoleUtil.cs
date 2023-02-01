76c7aa4d5dee23d8b0525f09498c26a2ebd5697a
EqUiNoX <87106037+EqUiNoX-Labs@users.noreply.github.com>
2023-02-01 13:00:43 -0800

    Initial commit

---
+++ POTify/ConsoleUtil.cs
@@ -0,0 +1,24 @@
+﻿using Mono.Options;
+
+namespace POTify
+{
+    public static class ConsoleUtil
+    {
+        public static void ShowHelpHeader(string version, OptionSet options)
+        {
+            Console.WriteLine("POTify {0}", version);
+            Console.WriteLine("POTify by EqUiNoX, texture power of 2 coverter.");
+            Console.WriteLine();
+            Console.WriteLine("Usage: POTify [options]+");
+            Console.WriteLine();
+            options.WriteOptionDescriptions(Console.Out);
+        }
+
+        public static void ShowOptionException(OptionException optionException)
+        {
+            Console.Write("POTify by EqUiNoX: ");
+            Console.WriteLine(optionException.Message);
+            Console.WriteLine("Try `POTify --help' for more information.");
+        }
+    }
+}
---
+++ POTify/Program.cs
@@ -0,0 +1,81 @@
+﻿using Mono.Options;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace POTify
+{
+    internal class Program
+    {
+        private const string Version = "1.0.0";
+
+        private static bool ShowHelp = false;
+        private static string Input = string.Empty;
+        private static string Output = string.Empty;
+
+        private static int CeilToNextPowerOfTwo(int value)
+        {
+            int powOfTwo = 1;
+            while (powOfTwo < value)
+            {
+                powOfTwo <<= 1;
+            }
+            return powOfTwo;
+        }
+
+        static void Main(string[] args)
+        {
+            var options = new OptionSet {
+                { "i|input=", $"Input folder", i => Input = i },
+                { "h|help", "show help", h => ShowHelp = h != null },
+            };
+
+            try
+            {
+                options.Parse(args);
+                if (ShowHelp && args.Length == 1)
+                {
+                    ConsoleUtil.ShowHelpHeader(Version, options);
+                    return;
+                }
+
+                var input = Path.GetFullPath(Input);
+                if (!Directory.Exists(input))
+                {
+                    throw new OptionException("Input is not a valid folder.", "input");
+                }
+
+                var output = Path.GetFullPath(Output);
+                if (!Directory.Exists(output))
+                {
+                    throw new OptionException("Output is not a valid folder.", "output");
+                }
+
+                var acceptedFiletypes = new string[] { ".png", ".jpg" };
+                var processFiles = Directory.GetFileSystemEntries(input, "*", new EnumerationOptions { IgnoreInaccessible = true, RecurseSubdirectories = false, MatchCasing = MatchCasing.CaseInsensitive })
+                    .Where(file => acceptedFiletypes.Contains(Path.GetExtension(file), StringComparer.CurrentCultureIgnoreCase))
+                    .ToList();
+
+                foreach ( var file in processFiles)
+                {
+                    var filename = Path.GetFileName(file);
+                    Console.WriteLine($"Processing file '{filename}'");
+                    var outputFile = Path.Combine(output, filename);
+                    using var image = Image.Load<Rgba32>(file);
+                    var newWidth = CeilToNextPowerOfTwo(image.Width);
+                    var newHeight = CeilToNextPowerOfTwo(image.Height);
+                    image.Mutate(x => { x.Resize(newWidth, newHeight); });
+                    image.Save(outputFile);
+                }
+            }
+            catch (OptionException e)
+            {
+                ConsoleUtil.ShowOptionException(e);
+                return;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Done.");
+        }
+    }
+}
