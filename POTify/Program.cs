2dcd29dc06ffc0ad1e577ff5e956109197f6c0c5
EqUiNoX <87106037+EqUiNoX-Labs@users.noreply.github.com>
2023-02-01 19:54:33 -0800

    Update: added round to nearest + recursion

--- POTify/Program.cs
+++ POTify/Program.cs
@@ -7,11 +7,21 @@ namespace POTify
 {
     internal class Program
     {
-        private const string Version = "1.0.1";
+        private const string Version = "1.0.2";
 
         private static bool ShowHelp = false;
         private static string Input = string.Empty;
         private static string Output = string.Empty;
+        private static bool CreateOutput = false;
+        private static bool RoundToNearest = false;
+
+        public static int RoundToNextPowerOfTwo(int value)
+        {
+            int next = CeilToNextPowerOfTwo(value);
+            int prev = next >> 1;
+            return next - value <= value - prev ? next : prev;
+        }
+
 
         private static int CeilToNextPowerOfTwo(int value)
         {
@@ -27,8 +37,10 @@ namespace POTify
         {
             var options = new OptionSet {
                 { "i|input=", $"Input folder", i => Input = i },
-                { "o|output=", $"Input folder", o => Output = o },
-                { "h|help", "show help", h => ShowHelp = h != null },
+                { "o|output=", $"Output folder", o => Output = o },
+                { "c|create", "Create output folder if does not exist", c => CreateOutput = c != null },
+                { "n|nearest", "Round to nearest power of two", n => RoundToNearest = n != null },
+                { "h|help", "Show help", h => ShowHelp = h != null },
             };
 
             try
@@ -49,23 +61,33 @@ namespace POTify
                 var output = Path.GetFullPath(Output);
                 if (!Directory.Exists(output))
                 {
-                    throw new OptionException("Output is not a valid folder.", "output");
+                    if (CreateOutput)
+                    {
+                        Directory.CreateDirectory(output);
+                    }
+                    else
+                    {
+                        throw new OptionException("Output is not a valid folder.", "output");
+                    }
                 }
 
                 var acceptedFiletypes = new string[] { ".png", ".jpg" };
-                var processFiles = Directory.GetFileSystemEntries(input, "*", new EnumerationOptions { IgnoreInaccessible = true, RecurseSubdirectories = false, MatchCasing = MatchCasing.CaseInsensitive })
+                var processFiles = Directory.GetFileSystemEntries(input, "*", new EnumerationOptions { IgnoreInaccessible = true, RecurseSubdirectories = true, MatchCasing = MatchCasing.CaseInsensitive })
                     .Where(file => acceptedFiletypes.Contains(Path.GetExtension(file), StringComparer.CurrentCultureIgnoreCase))
                     .ToList();
 
                 foreach ( var file in processFiles)
                 {
+                    var outputFolder = Path.GetDirectoryName(file.Replace(input, ""));
                     var filename = Path.GetFileName(file);
                     Console.WriteLine($"Processing file '{filename}'");
-                    var outputFile = Path.Combine(output, filename);
+                    var outputPath = $"{output}{outputFolder}";
+                    var outputFile = Path.Combine(outputPath, filename);
                     using var image = Image.Load<Rgba32>(file);
-                    var newWidth = CeilToNextPowerOfTwo(image.Width);
-                    var newHeight = CeilToNextPowerOfTwo(image.Height);
+                    var newWidth = RoundToNearest ? RoundToNextPowerOfTwo(image.Width) : CeilToNextPowerOfTwo(image.Width);
+                    var newHeight = RoundToNearest ? RoundToNextPowerOfTwo(image.Height) : CeilToNextPowerOfTwo(image.Height);
                     image.Mutate(x => { x.Resize(newWidth, newHeight); });
+                    Directory.CreateDirectory(outputPath);  
                     image.Save(outputFile);
                 }
             }
